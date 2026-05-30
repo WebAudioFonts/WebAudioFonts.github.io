54c92badc77ccd52a453003379a6483eadefbabf
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-05-30 03:17:36 -0400

    add sf2-json

--- src/scripts/includes/demo.js
+++ src/scripts/includes/demo.js
@@ -1,4 +1,4 @@
-import MidiAudioPlayer from "../libraries/midi-audio-player";
+import MidiAudioPlayer from "midi-audio-player";
 import DNDZone from "../libraries/dndzone";
 import DBMeter from "../libraries/dbmeter";
 import Slider from "../libraries/slider";
