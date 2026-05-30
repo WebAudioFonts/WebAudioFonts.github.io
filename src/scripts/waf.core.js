85796c56b32ba43b080110dfcb1f764b5e6815a4
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-05-29 21:17:13 -0400

    Avancement... tranquillement...

--- src/scripts/waf.core.js
+++ src/scripts/waf.core.js
@@ -3,9 +3,11 @@ import DEMO from './includes/demo';
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import bash from 'highlight.js/lib/languages/bash';
+import json from 'highlight.js/lib/languages/json';
 import css from 'highlight.js/lib/languages/css';
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('json', json);
 hljs.registerLanguage('css', css);
 
 
